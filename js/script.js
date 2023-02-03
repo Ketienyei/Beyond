ae85aeaa71f6d2aeeab45ea3e8d70bedbbc7d762
Cynthia Ketienyei <cynthiachptoo@gmail.com>
2023-02-03 10:48:03 +0300

    adding functionality on navbar

---
+++ js/script.js
@@ -0,0 +1,7 @@
+let navigation_bar = document.querySelector('navbar');
+let button = document.querySelector('btn');
+
+
+button.addEventListener('click', function(){
+    navigation_bar.classList.add('active?');
+})
