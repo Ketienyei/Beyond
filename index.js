65ab0a9a387f5f5918607ad2d2268a726ef74bee
Cynthia Ketienyei <cynthiachptoo@gmail.com>
2023-02-24 17:21:09 +0300

    still working on the back end

--- index.js
+++ index.js
@@ -1,8 +1,20 @@
 import express from "express"
 import dotenv from "dotenv"
+import mongoose from "mongoose"
 const app = express()
 dotenv.config()
 
+const connect = async () => {
+try {
+    await mongoose.set("strictQuery", false);
+    mongoose.connect(process.env.MONGO);
+    console.log("Conected to MONGODB")
+}catch(error) {
+    throw error;
+}
+};
+
 app.listen (8800,()=>{
-    console.log("Connected to backend")
-})
+    connect()
+    console.log("Connected to backend");
+});
