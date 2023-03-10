ecdbe6d052cce3e397752b606b2f55db49335101
Cynthia Ketienyei <cynthiachptoo@gmail.com>
2023-03-10 16:06:58 +0300

    coding

--- js/script.js
+++ js/script.js
@@ -5,3 +5,11 @@ let button = document.querySelector('btn-btn');
 button.addEventListener('click', function(){
     navigation_bar.classList.add('active?')
 })
+
+
+
+var dd_main = document.querySelector(".dd_main");
+    
+dd_main.addEventListener("click", function(){
+    this.classList.toggle("active");
+})
