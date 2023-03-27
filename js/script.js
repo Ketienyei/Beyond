6ca584b51faee2d93c632715eb7a3363cd8f1788
Cynthia Ketienyei <cynthiachptoo@gmail.com>
2023-03-27 21:07:06 +0300

    getting started

--- js/script.js
+++ js/script.js
@@ -1,13 +1,4 @@
-const button = document.getElementById('btn');
-  button.addEventListener('click', function() {
-    // Add your functionality here
-    console.log('Button clicked!');
-  });
-
-
-
-var dd_main = document.querySelector(".dd_main");
-    
-dd_main.addEventListener("click", function(){
-    this.classList.toggle("active");
-})
+const btn = document.getElementsid('btn');
+btn.addEventListener('click', function() {
+  alert('Button clicked!');
+});
